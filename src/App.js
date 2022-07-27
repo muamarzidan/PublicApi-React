605cd237cf53e99f79deee1c17a35a81a35fe1b5
muamarzidan <zidanprofix93@gmail.com>
2022-07-27 11:20:51 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [categories, setCategories] = useState();
+  const [search, setSearch] = useState();
+  const [dataSearched, setDataSearched] = useState();
+
+    useEffect(() => {
+      retrieveCategories()
+    }, []);
+
+  const retrieveCategories = async () => {
+    try {
+      const { data } = await axios.get('https://api.publicapis.org/categories');
+      console.log(data);
+      // setCategories(data);
+
+    } catch(err) {
+      console.log(err, '<== error retrieve categories');
+    }
+
+    // const handleChange = (e) => {
+    //   setSearch(e.target.value)
+    // }
+    // const handleSubmit = async (e) => {
+    //   try {
+    //     e.preventDefault();
+        
+    //     const { data } = await axios.get('https://api.publicapis.org/entries?category=${search}')
+    //     console.log(data, '<== response handle submit');
+
+    //   } catch(err) {
+    //     console.log(err, '<== error handle submit');
+    //   }
+    // }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* {JSON.stringify(categories)}  */}
+      <h1>Serach data API</h1> <br />
+
+      { /*<form>
+          <input/>
+          <button>Seacrh</button>
+        </form>*/ }
+      
+      {categories && categories.map((category, index) => {
+  <li key={index} > {category} </li>
+} )} 
     </div>
   );
 }
 
 export default App;
+
+{/* {JSON.stringify(categories)} onSubmit={handleSubmit} onChange={handleChange}*/}
+{/* <p>{categories}</p> */}
