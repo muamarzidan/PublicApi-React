8c1f01f22eb717d0ef2e4170680f02a582d5bdc0
muamarzidan <zidanprofix93@gmail.com>
2022-07-27 11:37:16 +0700

    2 commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 
 function App() {
 
-  const [categories, setCategories] = useState();
+  const [categories, setCategories] = useState([]);
   const [search, setSearch] = useState();
   const [dataSearched, setDataSearched] = useState();
 
@@ -16,47 +16,50 @@ function App() {
   const retrieveCategories = async () => {
     try {
       const { data } = await axios.get('https://api.publicapis.org/categories');
-      console.log(data);
-      // setCategories(data);
+      // console.log([data]);
+      setCategories([data]);
 
     } catch(err) {
       console.log(err, '<== error retrieve categories');
     }
 
-    // const handleChange = (e) => {
-    //   setSearch(e.target.value)
-    // }
-    // const handleSubmit = async (e) => {
-    //   try {
-    //     e.preventDefault();
+    const handleChange = (e) => {
+      setSearch(e.target.value)
+    }
+    const handleSubmit = async (e) => {
+      try {
+        e.preventDefault();
         
-    //     const { data } = await axios.get('https://api.publicapis.org/entries?category=${search}')
-    //     console.log(data, '<== response handle submit');
+        const { data } = await axios.get('https://api.publicapis.org/entries?category=${search}')
+        console.log(data, '<== response handle submit');
 
-    //   } catch(err) {
-    //     console.log(err, '<== error handle submit');
-    //   }
-    // }
+      } catch(err) {
+        console.log(err, '<== error handle submit');
+      }
+    }
   }
 
   return (
     <div className="App">
-      {/* {JSON.stringify(categories)}  */}
+
       <h1>Serach data API</h1> <br />
 
-      { /*<form>
+      <form>
           <input/>
           <button>Seacrh</button>
-        </form>*/ }
+      </form>
       
       {categories && categories.map((category, index) => {
   <li key={index} > {category} </li>
-} )} 
+} )}
     </div>
   );
 }
 
 export default App;
 
+
+
+{/* {JSON.stringify(categories)}  */}
 {/* {JSON.stringify(categories)} onSubmit={handleSubmit} onChange={handleChange}*/}
 {/* <p>{categories}</p> */}
