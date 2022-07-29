4aa7d41ae42e6bc9d5d5bd6d6c4f99aa4963695c
muamarzidan <zidanprofix93@gmail.com>
2022-07-29 09:49:05 +0700

    done-searc

--- src/App.js
+++ src/App.js
@@ -16,30 +16,28 @@ function App() {
     try {
       const { data } = await axios.get('https://api.publicapis.org/categories');
       setCategories(data.categories);
-      const { datasearch } = await axios.get('https://api.publicapis.org/entries?category=${search}');
-      setCategories(data.categories);
+    
       
     } catch(err) {
       console.log(err, '<== error retrieve categories');
     }
 
-    // const handleChange = (e) => {
-    //   setSearch(e.target.value)
-    // }
+    const handleChange = (e) => {
+      setSearch(e.target.value)
+    }
 
-    // const handleSubmit = async (e) => {
-    //   try {
-    //     e.preventDefault();
-    //     const { data } = await axios.get('https://api.publicapis.org/entries?category=${search}')
-    //     // console.log(data, '<== response handle submit');
-    //     setDataSearched(data);
+    const handleSubmit = async (e) => {
+      try {
+        e.preventDefault();
+        const { data } = await axios.get('https://api.publicapis.org/entries?category=${search}')
+        console.log(data, '<== response handle submit');
+        setDataSearched(data.categories);
 
-    //   }catch(err) {
-    //     console.log(err, '<== error handle submit');
-    //   }
-    // }
+      }catch(err) {
+        console.log(err, '<== error handle submit');
+      }
+    }
   }
-
 
   return (
     <div className="App">
@@ -51,8 +49,9 @@ function App() {
         <br />
 
         {/* {JSON.stringify(categories)}  */}
-        {JSON.stringify(dataSearched)}
-        {/* <ul>
+        {/* {JSON.stringify(dataSearched)} */}
+        
+        <ul>
           {dataSearched && dataSearched.map((data, index) => {
             console.log(data, '<== data searched');
             return (
@@ -65,21 +64,22 @@ function App() {
               </>
             )
           })}
-        </ul> */}
+        </ul>
     </div>
   );
+
 }
 
 export default App;
 
 
-        {/* {JSON.stringify(dataSearched)}; */}
+{/* {JSON.stringify(dataSearched)}; */}
 
-        {/* <ul>
-          {categories && categories.map((category, index) => {
-            return <li key={index} > {category} </li>
-           })}
-        </ul> */}
+{/* <ul>
+{categories && categories.map((category, index) => {
+return <li key={index} > {category} </li>
+})}
+</ul> */}
 {/* {JSON.stringify(categories)}  */}
 {/* {JSON.stringify(categories)} onSubmit={handleSubmit} onChange={handleChange}*/}
 {/* <p>{categories}</p> */}
